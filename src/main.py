4a0fe9d42c1eff3f19cbb24fda7aec246971222d
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2021-08-07 19:18:06 -0400

    Added settings and refined command recognition

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 # Importing the needed libraries
 import os
 import discord
+from settings import Settings
 
 # Commands
 from commands.plotCommand import PlotCommand
@@ -15,13 +16,15 @@ bot = discord.Client()
 
 commands = [PlotCommand(['plot'])]
 
+settings = Settings()
+
 # Showing that the bot is ready
 
 
 @bot.event
 async def on_ready():
-    await bot.get_channel(843616163010314260).send("I'm Here")
-    print("XHacks2021 is up and ready to roll out")
+    await bot.get_channel(settings.getAttribute("announce_channel_id")).send(settings.getAttribute("ready_message"))
+    print(settings.getAttribute("ready_message"))
 
 
 # Whenever there's a message, this function runs
@@ -30,14 +33,20 @@ async def on_message(message):
     # Ensures that a user is saying the commands and not the bot
     if message.author == bot:
       return
-
+    
+    if len(message.content) > 0 and message.content[0] != settings.getAttribute("command_prefix"):
+      return
+    
+    # Check if the message was a command
+    potentialCmdName = message.content.split(' ')[0][1::]
+    
     done = False
-
     for cmd in commands:
       cmdNames = cmd.getNames()
       for cmdName in cmdNames:
-        if(message.content.startswith(cmdName)):
+        if(potentialCmdName == (cmdName)):
           await cmd.run(message)
+          print("Command run: " + potentialCmdName)
           done = True
           break
       if done:
