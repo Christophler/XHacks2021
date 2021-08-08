13c92b58762903c94d48ed54f0dc7fb9c8807ff4
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2021-08-07 21:25:49 -0400

    Working test command prints list of 7 latest high point of given stock

--- src/commands/testCommand.py
+++ src/commands/testCommand.py
@@ -9,7 +9,20 @@ class TestCommand:
     def getNames(self):
         return self.names
         
-    async def run(self, message): 
-        msg = self.settings.getAttribute("ready_message")
-        await message.channel.send(msg)
+    async def run(self, message, args): 
+        if len(args) == 1:            
+            stockName = args[0]
+            json = timeSeriesDailyAdjusted(stockName)
+            dayData = json['Time Series (Daily)']
+            tempMax = 7
+            points = []
+            c = 0
+            for date in dayData:
+                if c > tempMax:
+                    break
+                c+= 1
+                point = dayData[date]['2. high']
+                points.append(point)
+            print(points)
+            
         
